fdcea700879b2931a22f91cd0c0ba6e841c18b4f
Moritz Angermann <moritz.angermann@gmail.com>
2018-07-24 12:15:06 +0800

    align more with old default.nix

--- scripts/launch/connect-to-cluster/default.nix
+++ scripts/launch/connect-to-cluster/default.nix
@@ -6,7 +6,6 @@
 , topologyFile ? null
 , system ? builtins.currentSystem
 , pkgs ? import localLib.fetchNixPkgs { inherit system config; }
-, gitrev ? localLib.commitIdFromGitRepo ./../../../.git
 , walletListen ? "127.0.0.1:8090"
 , walletDocListen ? "127.0.0.1:8091"
 , ekgListen ? "127.0.0.1:8000"
@@ -18,6 +17,7 @@
 , debug ? false
 , disableClientAuth ? false
 , extraParams ? ""
+, iohkPkgs
 , useStackBinaries ? false
 }:
 
@@ -57,7 +57,6 @@ let
     x509gen = if useStackBinaries then "stack exec -- cardano-x509-certificates" else "${iohkPkgs.cardano-sl-tools}/bin/cardano-x509-certificates";
   };
   ifWallet = localLib.optionalString (executable == "wallet");
-  iohkPkgs = import ./../../../default.nix { inherit config system pkgs gitrev; };
   src = ./../../../.;
   topologyFileDefault = pkgs.writeText "topology-${environment}" ''
     wallet:
